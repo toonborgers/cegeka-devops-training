f40cceacb74fe23b35bde6254d70039910275d2e
Toon Borgers <borgers.toon@gmail.com>
2016-11-25 18:51:15 +0100

    fix workflow

--- dev/jenkins/workflow.groovy
+++ dev/jenkins/workflow.groovy
@@ -1,7 +1,7 @@
 stage name: 'test'
 node {
     git 'https://github.com/toonborgers/cegeka-devops-training'
-    docker.withImage('java:8-jre-alpine') {
+    docker.image('java:8-jre-alpine').inside {
         sh 'dev/gradlew -p dev clean test integrationTest'
     }
 }
